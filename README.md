eaf3b96e7972d643c3e0ba37473358c0c74f386d
Agustin Camino <agustin.camino@gmail.com>
2016-06-20 17:05:42 -0500

    Here be dragons

---
+++ README.md
@@ -0,0 +1,3 @@
+# Task Router with ASP.NET MVC
+
+Use Twilio to provide your user with multiple options through phone calls, so they can be assisted by an agent specialized in the chosen topic. This is basically a call center created with the Task Router API of Twilio. This example uses a PostgreSQL database to log phone calls which were not assisted.
---
+++ TaskRouter.Web.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TaskRouter.Web.Controllers;
+using TestStack.FluentMVCTesting;
+
+namespace TaskRouter.Web.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            var controller = new HomeController();
+            controller.WithCallTo(c => c.Index())
+                .ShouldRenderDefaultView();
+        }
+    }
+}
