9a1674de2588b4221e78c214a682133b1d342dc1
Agustin Camino <agustin.camino@gmail.com>
2016-06-22 17:03:45 -0500

    Update WorkspaceConfig
    Pull configuration from local config file.

--- TaskRouter.Web/App_Start/WorkspaceConfig.cs
+++ TaskRouter.Web/App_Start/WorkspaceConfig.cs
@@ -1,61 +1,78 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using TaskRouter.Web.Domain;
 using Twilio.TaskRouter;
 
 namespace TaskRouter.Web
 {
     public class WorkspaceConfig
     {
+        private readonly TaskRouterClient _client;
+
         public static void RegisterWorkspace()
         {
-            var client = new TaskRouterClient("accountSid", "authToken");
+            new WorkspaceConfig().Register();
+        }
 
-            // Workspace
-            var workspace = client.AddWorkspace("Twilio Workspace", "https://sb.ngrok.io/events", null);
+        public WorkspaceConfig()
+        {
+            _client = new TaskRouterClient(Config.AccountSID, Config.AuthToken);
+        }
+
+        public void Register()
+        {
+            var workspace = GetWorkspaceByFriendlyName("Twilio Workspace");
+            if (workspace != null)
+            {
+                _client.DeleteWorkspace(workspace.Sid);
+            }
+
+            workspace = _client.AddWorkspace("Twilio Workspace", "https://sb.ngrok.io/events", null);
             var workspaceSid = workspace.Sid;
 
-            var attributesForBob = new Dictionary<string, string>();
-            attributesForBob.Add("products", "[ProgrammableSMS]");
-            attributesForBob.Add("contact_uri", "+593992670240");
-            client.AddWorker(workspaceSid, "Bob", null, attributesForBob);
+            var attributesForBob = "{\"products\": [\"ProgrammableSMS\"], \"contact_uri\": \"+593992670240\"}";
+            var bob = _client.AddWorker(workspaceSid, "Bob", null, attributesForBob);
 
-            var attributesForAlice = new Dictionary<string, string>();
-            attributesForAlice.Add("products", "[ProgrammableVoice]");
-            attributesForAlice.Add("contact_uri", "+593987908027");
-            client.AddWorker(workspaceSid, "Alice", null, attributesForAlice);
+            var attributesForAlice = "{\"products\": [\"ProgrammableVoice\"], \"contact_uri\": \"+593987908027\"}";
+            var alice = _client.AddWorker(workspaceSid, "Alice", null, attributesForAlice);
 
+            var reservationActivity = GetActivityByFriendlyName(workspaceSid, "Reserved");
+            var assignmentActivity = GetActivityByFriendlyName(workspaceSid, "Busy");
 
-            // Task queues
-            var voiceQueue = client.AddTaskQueue(workspaceSid, "Voice", null, null, "products HAS 'ProgrammableVoice'", null);
-            var smsQueue = client.AddTaskQueue(workspaceSid, "SMS", null, null, "products HAS 'ProgrammableSMS'", null);
-            var allQueue = client.AddTaskQueue(workspaceSid, "SMS", null, null, "1==1", null);
+            var voiceQueue = _client.AddTaskQueue(
+                workspaceSid,
+                "Voice",
+                assignmentActivity.Sid,
+                reservationActivity.Sid,
+                "products HAS \"ProgrammableVoice\"", null);
+
+            var smsQueue = _client.AddTaskQueue(workspaceSid,
+                "SMS",
+                assignmentActivity.Sid,
+                reservationActivity.Sid,
+                "products HAS \"ProgrammableSMS\"", null);
+
+            var allQueue = _client.AddTaskQueue(
+                workspaceSid,
+                "All",
+                assignmentActivity.Sid,
+                reservationActivity.Sid,
+                "1==1", null);
 
             // Workflow
             var voiceFilter = new Filter()
             {
                 FriendlyName = "Voice",
-                Expression = "selected_product == 'ProgrammableVoice'",
-                Targets = new List<Target>()
-                {
-                    new Target
-                    {
-                        Queue = voiceQueue.Sid
-                    }
-                }
+                Expression = "selected_product==\"ProgrammableVoice\"",
+                Targets = new List<Target>() { new Target { Queue = voiceQueue.Sid } }
             };
 
             var smsFilter = new Filter()
             {
                 FriendlyName = "SMS",
-                Expression = "selected_product == 'ProgrammableSMS'",
-                Targets = new List<Target>()
-                {
-                    new Target
-                    {
-                        Queue = smsQueue.Sid
-                    }
-                }
+                Expression = "selected_product==\"ProgrammableSMS\"",
+                Targets = new List<Target>() { new Target { Queue = smsQueue.Sid } }
             };
 
             var workflowConfiguration = new WorkflowConfiguration();
@@ -66,15 +83,30 @@ namespace TaskRouter.Web
 
             // Convert to json
             var workflowJSON = workflowConfiguration.ToString();
+            workflowJSON = "{\"task_routing\":" + workflowJSON + "}";
 
             // Call rest api
-            Workflow workflow = client.AddWorkflow(
-                workspaceSid, "Tech Support", workflowJSON, "https://sb.ngrok.io/assignment", null, 60);
+            Workflow workflow = _client.AddWorkflow(
+                workspaceSid,
+                "Tech Support",
+                workflowJSON,
+                "https://sb.ngrok.io/assignment",
+                "https://sb.ngrok.io/assignment",
+                60);
 
             Console.WriteLine(workflow.FriendlyName);
+        }
 
-            var activityResult = client.ListActivities(workspace.Sid);
-            var activity = activityResult.Activities.First(a => a.FriendlyName == "Idle");
+        private Activity GetActivityByFriendlyName(string workspaceSid, string friendlyName)
+        {
+            var activityResult = _client.ListActivities(workspaceSid);
+            return activityResult.Activities.First(a => a.FriendlyName == friendlyName);
+        }
+
+        private Workspace GetWorkspaceByFriendlyName(string friendlyName)
+        {
+            var workspaceResult = _client.ListWorkspaces();
+            return workspaceResult.Workspaces.FirstOrDefault(w => w.FriendlyName == friendlyName);
         }
     }
 }
